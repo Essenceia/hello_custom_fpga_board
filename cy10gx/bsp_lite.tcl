1b714ad7e350313ec54d6b8d2d5fd8fd43d82b6f
Julia Desmazes <julia.desmazes@gmail.com>
2023-12-30 14:40:56 -0600

    RTL,BSP: Wrote small led blink module in top, not linted

--- cy10gx/bsp_lite.tcl
+++ cy10gx/bsp_lite.tcl
@@ -6,44 +6,6 @@ set_global_assignment -name MAX_CORE_JUNCTION_TEMP 100
 set_global_assignment -name DEVICE_FILTER_PIN_COUNT 780
 set_global_assignment -name ERROR_CHECK_FREQUENCY_DIVISOR 1
 
-#[Transceiver Ref Clocks]
-set_location_assignment PIN_R24 -to GXB1D_125M
-set_location_assignment PIN_N24 -to GXB1D_644M
-set_location_assignment PIN_R23 -to "GXB1D_125M_N"
-set_location_assignment PIN_N23 -to "GXB1D_644M_N"
-set_instance_assignment -name IO_STANDARD LVDS -to GXB1D_125M -entity top
-set_instance_assignment -name IO_STANDARD LVDS -to GXB1D_644M -entity top
-
-#[Transceiver Lanes (Bank 1D)]
-set_location_assignment PIN_P26 -to GXB1D_RXD[0]
-set_location_assignment PIN_M26 -to GXB1D_RXD[1]
-set_location_assignment PIN_K26 -to GXB1D_RXD[2]
-set_location_assignment PIN_H26 -to GXB1D_RXD[3]
-set_location_assignment PIN_F26 -to GXB1D_RXD[4]
-set_location_assignment PIN_D26 -to GXB1D_RXD[5]
-set_location_assignment PIN_D25 -to "GXB1D_RXD_N[5]"
-set_location_assignment PIN_F25 -to "GXB1D_RXD_N[4]"
-set_location_assignment PIN_H25 -to "GXB1D_RXD_N[3]"
-set_location_assignment PIN_K25 -to "GXB1D_RXD_N[2]"
-set_location_assignment PIN_M25 -to "GXB1D_RXD_N[1]"
-set_location_assignment PIN_P25 -to "GXB1D_RXD_N[0]"
-set_instance_assignment -name IO_STANDARD "HIGH SPEED DIFFERENTIAL I/O" -to GXB1D_RXD[*] -entity top
-set_instance_assignment -name XCVR_VCCR_VCCT_VOLTAGE 0_9V -to GXB1D_TXD[0] -entity top
-
-set_location_assignment PIN_N28 -to GXB1D_TXD[1]
-set_location_assignment PIN_L28 -to GXB1D_TXD[2]
-set_location_assignment PIN_J28 -to GXB1D_TXD[3]
-set_location_assignment PIN_G28 -to GXB1D_TXD[4]
-set_location_assignment PIN_E28 -to GXB1D_TXD[5]
-set_location_assignment PIN_E27 -to "GXB1D_TXD_N[5]"
-set_location_assignment PIN_G27 -to "GXB1D_TXD_N[4]"
-set_location_assignment PIN_J27 -to "GXB1D_TXD_N[3]"
-set_location_assignment PIN_L27 -to "GXB1D_TXD_N[2]"
-set_location_assignment PIN_N27 -to "GXB1D_TXD_N[1]"
-set_location_assignment PIN_R27 -to "GXB1D_TXD_N[0]"
-set_instance_assignment -name IO_STANDARD "HIGH SPEED DIFFERENTIAL I/O" -to GXB1D_TXD[*] -entity top
-set_instance_assignment -name XCVR_VCCR_VCCT_VOLTAGE 0_9V -to GXB1D_TXD[*] -entity top
-
 #[3.0V I/O]
 set_location_assignment PIN_A24 -to OSC_50m
 set_instance_assignment -name IO_STANDARD "1.8 V" -to OSC_50m -entity top
@@ -51,10 +13,18 @@ set_instance_assignment -name IO_STANDARD "1.8 V" -to OSC_50m -entity top
 set_location_assignment PIN_E22 -to FPGA_RSTn
 set_instance_assignment -name IO_STANDARD "3.0-V LVCMOS" -to FPGA_RSTn -entity top
 
-# Unusued 
-
-# GX 1D channels [0:3] and 5
-set_global_assignment -name PRESERVE_UNUSED_XCVR_CHANNEL ON
+# Led
+set_location_assignment PIN_B8 -to USER_LED[0]
+set_location_assignment PIN_A8 -to USER_LED[1]
+set_location_assignment PIN_B9 -to USER_LED[2]
+set_location_assignment PIN_A9 -to USER_LED[3]
+set_location_assignment PIN_B10 -to USER_LED[4]
+set_location_assignment PIN_B11 -to USER_LED[5]
+set_location_assignment PIN_A11 -to USER_LED[6]
+set_location_assignment PIN_A12 -to USER_LED[7]
+set_location_assignment PIN_A13 -to USER_LED[8]
+set_location_assignment PIN_B13 -to USER_LED[9]
+set_instance_assignment -name IO_STANDARD "1.8 V" -to "USER_LED[*]"
 
 # Thermal
 set_global_assignment -name POWER_PRESET_COOLING_SOLUTION "23 MM HEAT SINK WITH 200 LFPM AIRFLOW"
